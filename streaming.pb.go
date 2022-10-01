1da07c949688344b31eba329dcc2aa98907f68ce
Hamdi Ahmadi Muzakkiy <hamdiahmadi1504@gmail.com>
2022-10-01 20:27:02 +0700

    update proto

--- streaming.pb.go
+++ streaming.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.26.0
 // 	protoc        v3.17.1
-// source: proto/streaming.proto
+// source: streaming.proto
 
 package proto
 
@@ -31,7 +31,7 @@ type KeywordsData struct {
 func (x *KeywordsData) Reset() {
 	*x = KeywordsData{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_proto_streaming_proto_msgTypes[0]
+		mi := &file_streaming_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -44,7 +44,7 @@ func (x *KeywordsData) String() string {
 func (*KeywordsData) ProtoMessage() {}
 
 func (x *KeywordsData) ProtoReflect() protoreflect.Message {
-	mi := &file_proto_streaming_proto_msgTypes[0]
+	mi := &file_streaming_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -57,7 +57,7 @@ func (x *KeywordsData) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use KeywordsData.ProtoReflect.Descriptor instead.
 func (*KeywordsData) Descriptor() ([]byte, []int) {
-	return file_proto_streaming_proto_rawDescGZIP(), []int{0}
+	return file_streaming_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *KeywordsData) GetKeyword() []string {
@@ -71,12 +71,14 @@ type Input struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
+
+	Size int64 `protobuf:"varint,1,opt,name=size,proto3" json:"size,omitempty"`
 }
 
 func (x *Input) Reset() {
 	*x = Input{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_proto_streaming_proto_msgTypes[1]
+		mi := &file_streaming_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -89,7 +91,7 @@ func (x *Input) String() string {
 func (*Input) ProtoMessage() {}
 
 func (x *Input) ProtoReflect() protoreflect.Message {
-	mi := &file_proto_streaming_proto_msgTypes[1]
+	mi := &file_streaming_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -102,43 +104,51 @@ func (x *Input) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Input.ProtoReflect.Descriptor instead.
 func (*Input) Descriptor() ([]byte, []int) {
-	return file_proto_streaming_proto_rawDescGZIP(), []int{1}
+	return file_streaming_proto_rawDescGZIP(), []int{1}
 }
 
-var File_proto_streaming_proto protoreflect.FileDescriptor
+func (x *Input) GetSize() int64 {
+	if x != nil {
+		return x.Size
+	}
+	return 0
+}
 
-var file_proto_streaming_proto_rawDesc = []byte{
-	0x0a, 0x15, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x73, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x69, 0x6e,
-	0x67, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x28, 0x0a, 0x0c, 0x4b, 0x65, 0x79, 0x77, 0x6f,
-	0x72, 0x64, 0x73, 0x44, 0x61, 0x74, 0x61, 0x12, 0x18, 0x0a, 0x07, 0x6b, 0x65, 0x79, 0x77, 0x6f,
-	0x72, 0x64, 0x18, 0x01, 0x20, 0x03, 0x28, 0x09, 0x52, 0x07, 0x6b, 0x65, 0x79, 0x77, 0x6f, 0x72,
-	0x64, 0x22, 0x07, 0x0a, 0x05, 0x49, 0x6e, 0x70, 0x75, 0x74, 0x32, 0x39, 0x0a, 0x08, 0x4b, 0x65,
-	0x79, 0x77, 0x6f, 0x72, 0x64, 0x73, 0x12, 0x2d, 0x0a, 0x10, 0x53, 0x74, 0x72, 0x65, 0x61, 0x6d,
-	0x69, 0x6e, 0x67, 0x4b, 0x65, 0x79, 0x77, 0x6f, 0x72, 0x64, 0x12, 0x06, 0x2e, 0x49, 0x6e, 0x70,
-	0x75, 0x74, 0x1a, 0x0d, 0x2e, 0x4b, 0x65, 0x79, 0x77, 0x6f, 0x72, 0x64, 0x73, 0x44, 0x61, 0x74,
-	0x61, 0x22, 0x00, 0x30, 0x01, 0x42, 0x1a, 0x5a, 0x18, 0x2e, 0x2e, 0x2f, 0x67, 0x72, 0x70, 0x63,
-	0x2d, 0x74, 0x65, 0x73, 0x74, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x3b, 0x70, 0x72, 0x6f, 0x74,
-	0x6f, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+var File_streaming_proto protoreflect.FileDescriptor
+
+var file_streaming_proto_rawDesc = []byte{
+	0x0a, 0x0f, 0x73, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x69, 0x6e, 0x67, 0x2e, 0x70, 0x72, 0x6f, 0x74,
+	0x6f, 0x22, 0x28, 0x0a, 0x0c, 0x4b, 0x65, 0x79, 0x77, 0x6f, 0x72, 0x64, 0x73, 0x44, 0x61, 0x74,
+	0x61, 0x12, 0x18, 0x0a, 0x07, 0x6b, 0x65, 0x79, 0x77, 0x6f, 0x72, 0x64, 0x18, 0x01, 0x20, 0x03,
+	0x28, 0x09, 0x52, 0x07, 0x6b, 0x65, 0x79, 0x77, 0x6f, 0x72, 0x64, 0x22, 0x1b, 0x0a, 0x05, 0x49,
+	0x6e, 0x70, 0x75, 0x74, 0x12, 0x12, 0x0a, 0x04, 0x73, 0x69, 0x7a, 0x65, 0x18, 0x01, 0x20, 0x01,
+	0x28, 0x03, 0x52, 0x04, 0x73, 0x69, 0x7a, 0x65, 0x32, 0x39, 0x0a, 0x08, 0x4b, 0x65, 0x79, 0x77,
+	0x6f, 0x72, 0x64, 0x73, 0x12, 0x2d, 0x0a, 0x10, 0x53, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x69, 0x6e,
+	0x67, 0x4b, 0x65, 0x79, 0x77, 0x6f, 0x72, 0x64, 0x12, 0x06, 0x2e, 0x49, 0x6e, 0x70, 0x75, 0x74,
+	0x1a, 0x0d, 0x2e, 0x4b, 0x65, 0x79, 0x77, 0x6f, 0x72, 0x64, 0x73, 0x44, 0x61, 0x74, 0x61, 0x22,
+	0x00, 0x30, 0x01, 0x42, 0x1a, 0x5a, 0x18, 0x2e, 0x2e, 0x2f, 0x67, 0x72, 0x70, 0x63, 0x2d, 0x74,
+	0x65, 0x73, 0x74, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x3b, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62,
+	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_proto_streaming_proto_rawDescOnce sync.Once
-	file_proto_streaming_proto_rawDescData = file_proto_streaming_proto_rawDesc
+	file_streaming_proto_rawDescOnce sync.Once
+	file_streaming_proto_rawDescData = file_streaming_proto_rawDesc
 )
 
-func file_proto_streaming_proto_rawDescGZIP() []byte {
-	file_proto_streaming_proto_rawDescOnce.Do(func() {
-		file_proto_streaming_proto_rawDescData = protoimpl.X.CompressGZIP(file_proto_streaming_proto_rawDescData)
+func file_streaming_proto_rawDescGZIP() []byte {
+	file_streaming_proto_rawDescOnce.Do(func() {
+		file_streaming_proto_rawDescData = protoimpl.X.CompressGZIP(file_streaming_proto_rawDescData)
 	})
-	return file_proto_streaming_proto_rawDescData
+	return file_streaming_proto_rawDescData
 }
 
-var file_proto_streaming_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
-var file_proto_streaming_proto_goTypes = []interface{}{
+var file_streaming_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_streaming_proto_goTypes = []interface{}{
 	(*KeywordsData)(nil), // 0: KeywordsData
 	(*Input)(nil),        // 1: Input
 }
-var file_proto_streaming_proto_depIdxs = []int32{
+var file_streaming_proto_depIdxs = []int32{
 	1, // 0: Keywords.StreamingKeyword:input_type -> Input
 	0, // 1: Keywords.StreamingKeyword:output_type -> KeywordsData
 	1, // [1:2] is the sub-list for method output_type
@@ -148,13 +158,13 @@ var file_proto_streaming_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_proto_streaming_proto_init() }
-func file_proto_streaming_proto_init() {
-	if File_proto_streaming_proto != nil {
+func init() { file_streaming_proto_init() }
+func file_streaming_proto_init() {
+	if File_streaming_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_proto_streaming_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_streaming_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*KeywordsData); i {
 			case 0:
 				return &v.state
@@ -166,7 +176,7 @@ func file_proto_streaming_proto_init() {
 				return nil
 			}
 		}
-		file_proto_streaming_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+		file_streaming_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*Input); i {
 			case 0:
 				return &v.state
@@ -183,18 +193,18 @@ func file_proto_streaming_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_proto_streaming_proto_rawDesc,
+			RawDescriptor: file_streaming_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
-		GoTypes:           file_proto_streaming_proto_goTypes,
-		DependencyIndexes: file_proto_streaming_proto_depIdxs,
-		MessageInfos:      file_proto_streaming_proto_msgTypes,
+		GoTypes:           file_streaming_proto_goTypes,
+		DependencyIndexes: file_streaming_proto_depIdxs,
+		MessageInfos:      file_streaming_proto_msgTypes,
 	}.Build()
-	File_proto_streaming_proto = out.File
-	file_proto_streaming_proto_rawDesc = nil
-	file_proto_streaming_proto_goTypes = nil
-	file_proto_streaming_proto_depIdxs = nil
+	File_streaming_proto = out.File
+	file_streaming_proto_rawDesc = nil
+	file_streaming_proto_goTypes = nil
+	file_streaming_proto_depIdxs = nil
 }
